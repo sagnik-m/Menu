64654895d2411b04ff1e20dc390a7a9abaf5f395
Sagnik Majumdar <sagnik_m@ec.iitr.ac.in>
2022-12-19 01:51:08 +0530

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-           selectedDish: null
-        };
+
     }
 
-    onDishSelect(dish){
-        this.setState({selectedDish: dish});
-    }
+    //this portion deals with the details of each menu item. So this is now gonna be dealt using DishDetail
+    /*
     renderDish(dish){
         if(dish!=null){
             return(
@@ -31,12 +29,13 @@ class Menu extends Component {
             );
         }
     }
+    */
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={()=> this.onDishSelect(dish)}>
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card onClick={()=> this.props.onClick(dish.id)}>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -50,11 +49,6 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                  
-                    {this.renderDish(this.state.selectedDish)}
-                
                 </div>
             </div>
         );
